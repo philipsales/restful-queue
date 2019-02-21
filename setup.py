f0b1dc57aa600f8d5e943be39abcbc315fb84a8e
philipsales <pj_sal87@yahoo.com>
2019-02-21 18:49:03 +0800

    Update files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import sys
 from setuptools import setup, find_packages
 
-NAME = "flask_api_server"
+NAME = "resthooks_server"
 VERSION = "1.0.0"
 
 # To install the library, run the following
@@ -18,16 +18,16 @@ REQUIRES = ["connexion"]
 setup(
     name=NAME,
     version=VERSION,
-    description="Swagger ReST Article",
+    description="RESTHooks Article",
     author_email="",
     url="",
-    keywords=["Swagger", "Swagger ReST Article"],
+    keywords=["Swagger", "RESTHooks"],
     install_requires=REQUIRES,
     packages=find_packages(),
-    package_data={'': ['flask_api_server/swagger.yaml']},
+    package_data={'': ['resthooks_server/swagger.yaml']},
     include_package_data=True,
     entry_points={
-        'console_scripts': ['flask_api_server=flask_api_server.__main__:main']},
+        'console_scripts': ['resthooks_server=resthooks_server.__main__:main']},
     long_description="""\
     This is the swagger file that goes with our server code
     """
